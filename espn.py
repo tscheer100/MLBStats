70c68860e2f74b9ed9d2f2acae44ee02e35db3b5
tscheer100 <54217979+tscheer100@users.noreply.github.com>
2020-02-08 20:38:08 -0600

    Code now extracts data from all pages and gives espn.com a break.

--- espn.py
+++ espn.py
@@ -1,25 +1,42 @@
 import requests
 from bs4 import BeautifulSoup
+import time
 
-
-#get website, trick website into thinking you're using chrome.
-url = 'http://www.espn.com/mlb/stats/pitching/_/sort/wins/league/al/year/2019/seasontype/2'
-headers ={'User-Agent': 'Mozilla/5.0'}
-res = requests.get(url, headers)
-
-#sets soup to res and spits it out in html parsing format
-soup = BeautifulSoup(res.content, 'html.parser')
-
-#finds table from website
-stats = soup.find_all('table', class_='tablehead')
-stats = stats[0]
-
-#saves the table into a text file.
 with open('pitchers_stats.txt', 'w') as r:
- for row in stats.find_all('tr'):
-    for cell in row.find_all('td'):
-        r.write(cell.text.ljust(18))
-    # divide each row by a new line
-    r.write('\n')
+    r.write('Pitcher Statistics by Ranking.\n\n')
+
+num = 1
+
+#gets the first page of picture stats from espn, trick website into thinking you're using chrome.
+url = 'http://www.espn.com/mlb/stats/pitching/_/league/al/sort/wins/count/{}/qualified/false'.format(num)
+headers ={'User-Agent': 'Mozilla/5.0'}
+
+#goes to the next page over and over again until it reaches the last pitcher
+while num <= 417:
+    url = 'http://www.espn.com/mlb/stats/pitching/_/league/al/sort/wins/count/{}/qualified/false'.format(num)
+    time.sleep(1)
+    res = requests.get(url, headers)
+    #if the page returns correctly
+    if res.status_code == 200:
+        soup = BeautifulSoup(res.content, 'html.parser')
+        stats = soup.find('table', class_='tablehead')
+        # saves the table into a text file.
+        with open('pitchers_stats.txt', 'a') as r:
+            for row in stats.find_all('tr'):
+                for cell in row.find_all('td'):
+                    r.write(cell.text.ljust(25))
+                # divide each row by a new line
+                r.write('\n')
+    else:
+        print("no response")
+        print(num)
+    num += 40
+
+
+
+
+
+
+
 
 
